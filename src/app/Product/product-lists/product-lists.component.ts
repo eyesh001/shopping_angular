f99998cf5b5574f7dc21e5af39a24321876f0fe8
eyesh001 <eyesh001@naver.com>
2020-08-08 16:46:35 +0900

    productlists front change

--- src/app/Product/product-lists/product-lists.component.ts
+++ src/app/Product/product-lists/product-lists.component.ts
@@ -7,14 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ProductListsComponent implements OnInit {
-  prodcutLists = [];
+  productLists = [];
 
   constructor() {
-    this.prodcutLists.push({id: 1, name: 'check1', img:'https://i.picsum.photos/id/1054/3079/1733.jpg?hmac=Rk5_Xt3oLlDLJHH3ZDyHCqua0s45mhNjXmID277ZOMI'});
-    this.prodcutLists.push({id: 2, name: 'check2'});
+    this.productLists.push({id: 0, name: 'check1', img:'https://i.picsum.photos/id/1054/3079/1733.jpg?hmac=Rk5_Xt3oLlDLJHH3ZDyHCqua0s45mhNjXmID277ZOMI'});
+    this.productLists.push({id: 1, name: 'check2'}),
+    this.productLists.push({id: 2, name: 'check3'}),
+    this.productLists.push({id: 3, name: 'check4'}),
+    this.productLists.push({id: 4, name: 'check5'}),
+    this.productLists.push({id: 5, name: 'check6'}),
+    this.productLists.push({id: 6, name: 'check7'});
+  }
+
+  productDetailOpen(productList: String[]){
+    console.log(productList);
   }
 
   ngOnInit() {
-    console.log('checkddd');
   }
 }
